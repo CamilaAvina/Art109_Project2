cf355c98c31e4d7c0eacde5911b7904119385130
Camila Avina <twin2avina@gmail.com>
2021-10-25 12:17:32 -0700

    editing

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 //p5.SoundFile example 0
 //preload example from https://p5js.org/reference/#/p5.SoundFile to be familiar to how to add soun
+//http://molleindustria.github.io/p5.play/examples/index.html
 let mySound;
-var cart;
+  var kart;
 function preload() {
   soundFormats('mp3', 'ogg');
   mySound = loadSound('asset/register.mp3');
-  cart = loadAnimation('asset/shoppingkart.png', 'asset/shoppingkart2.png');
+  kart= loadAnimation('asset/shoppingkart.png', 'asset/shoppingkart2.png');
+
 }
 
 function setup() {
@@ -17,8 +19,11 @@ function setup() {
 function draw(){
   background(125, 206, 160);
   textSize(16);
-  text('tap here to pay', 10, 20);
-  animation(cart, 300, 150);
+  text(' pay', 10, 20);
+  textSize(16);
+  text('add the amount selected',250,500);
+  animation(kart, 300, 150);
+
 }
 
 function canvasPressed() {
@@ -31,9 +36,10 @@ function setup() {
   createCanvas(100, 100);
   background('grey');
   let inp = createInput('');
-  inp.position(150, 600);
+  inp.position(150, 850);
   inp.size(100);
   inp.input(myInputEvent);
+
 }
 
 function myInputEvent() {
